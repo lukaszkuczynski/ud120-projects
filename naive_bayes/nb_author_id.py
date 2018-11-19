c6516d1b3b9f8969ed4d55996d147c78b430d588
kuczynskilukasz <kuczynskilukasz@gmail.com>
2018-11-19 12:04:36 +0100

    Bayes measuring time

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -24,12 +24,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 
 clf = GaussianNB()
+
 t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
 from sklearn.metrics import accuracy_score
 
+t1 = time()
 y_pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
 score = accuracy_score(labels_test, y_pred)
 print(score)
 
