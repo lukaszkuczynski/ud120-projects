60ed3d5c2aed7267f6393455313fdf32ac06103b
kuczynskilukasz <kuczynskilukasz@gmail.com>
2018-11-19 13:35:31 +0100

    3.36 Predictions extracted for selected indices

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,16 +22,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-C_values = [10000]
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
-for C in C_values:
-    clf = SVC(kernel='rbf', C=C)
-    print("SVC with kernel rbf and C value %f" % C)
-    t0 = time()
-    clf.fit(features_train, labels_train)
-    print "training time:", round(time()-t0, 3), "s"
-    t1 = time()
-    y_pred = clf.predict(features_test)
-    print "prediction time:", round(time()-t1, 3), "s"
-    score = accuracy_score(labels_test, y_pred)
-    print(score)
+clf = SVC(kernel='rbf', C=10000)
+print("SVC with kernel rbf and C value 10000")
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+y_pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+score = accuracy_score(labels_test, y_pred)
+print(score)
+
+chosen_label_indices = [10, 26, 50]
+for index in chosen_label_indices:
+    print("For index %d we have label %d" %(index, y_pred[index]))
