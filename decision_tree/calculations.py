dcecb674c9c35fdfc23b78bba4afe1d2248f44fe
lukaszkuczynski <kuczynskilukasz@gmail.com>
2018-11-27 07:39:46 +0100

    [4.27] entropy

--- decision_tree/calculations.py
+++ decision_tree/calculations.py
@@ -10,3 +10,10 @@ from math import log
 entropy = -2.0/3.0 * log(2.0/3.0, 2) - 1.0/3.0 * log(1.0/3.0, 2)
 #entropy = 2 / 3.0
 print("entropy = %f" % entropy)
+
+
+entropy_parent = 1
+entropy_children = 3.0/4.0 * entropy + 1.0/4.0 * 0
+
+information_gain = entropy_parent - entropy_children
+print("information_gain = %f" % information_gain)
