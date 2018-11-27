21c49c669f0f1b77490f5c446f55ac9991d8ad58
lukaszkuczynski <kuczynskilukasz@gmail.com>
2018-11-27 07:58:00 +0100

    [4.37] Tree classification with score around .98

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -23,8 +23,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+### your code goes here ##
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+def train():
+    dtc = DecisionTreeClassifier(min_samples_split=40)
+    dtc.fit(features_train, labels_train)    
+    Y = dtc.predict(features_test)
+    score = accuracy_score(labels_test, Y)
+    print("Score is, my dear, %f" % score)
+
+train()
 
 #########################################################
 
