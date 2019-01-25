bddc93727b2be59a698db5a6c021bdef4a173032
lukaszkuczynski <kuczynskilukasz@gmail.com>
2019-01-25 11:23:27 +0100

    [15.31] number of TP

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -33,12 +33,12 @@ dtc = DecisionTreeClassifier()
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(features, labels, random_state=42, test_size=0.3)
-# dtc.fit(X_train, y_train)
+dtc.fit(X_train, y_train)
 
-# from sklearn.metrics import accuracy_score
-# y_pred = dtc.predict(X_test)
-# acc = accuracy_score(y_test, y_pred)
-# print("Accuracy is %.3f" % acc)
+from sklearn.metrics import accuracy_score
+y_pred = dtc.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print("Accuracy is %.3f" % acc)
 
 pois = filter(lambda x: x==1, y_test)
 print("We have %d POIs in the test set" % len(pois))
@@ -47,3 +47,8 @@ print("We have total of %d people in the test set" % len(y_test))
 true_not_poi = len(y_test) - len(pois)
 accuracy_for_all_zeros = true_not_poi / float(len(y_test))
 print("If an identifier predicted only zeroes we would have %.2f accuracy" % accuracy_for_all_zeros)
+
+from sklearn.metrics import confusion_matrix
+confu = confusion_matrix(y_test, y_pred)
+print(confu)
+print("True positives count is %d" % confu[1][1])
