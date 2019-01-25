f6d50d24aa637d5a051bfa14d1f65cda7ddfe008
lukaszkuczynski <kuczynskilukasz@gmail.com>
2019-01-25 11:16:00 +0100

    [15.30] judging accuracy of a very bad identifier

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -43,3 +43,7 @@ X_train, X_test, y_train, y_test = train_test_split(features, labels, random_sta
 pois = filter(lambda x: x==1, y_test)
 print("We have %d POIs in the test set" % len(pois))
 print("We have total of %d people in the test set" % len(y_test))
+
+true_not_poi = len(y_test) - len(pois)
+accuracy_for_all_zeros = true_not_poi / float(len(y_test))
+print("If an identifier predicted only zeroes we would have %.2f accuracy" % accuracy_for_all_zeros)
